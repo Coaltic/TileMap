8af5d23dca7aa0b1caefb5124875466574fc3a7c
Coaltic <glindsay156@gmail.com>
2021-04-06 16:00:08 -0300

    Interactivity and Skybox Added

---
+++ Assets/Scripts/InteractionObject.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionObject : MonoBehaviour
+{
+    [Header("Used for objects that give simple info about themselves")]
+    public bool info;
+    [TextArea]
+    public string[] messages;
+    public Text infoText;
+
+    [Header("This object can be picked up")]
+    [Space]
+    public bool pickup;
+    public bool gemPickup;
+
+    [Header("Used for NPC dialogue, with multiple screens")]
+    [Space]
+    public bool talks;
+    [TextArea]
+    public string[] sentences;
+    [TextArea]
+    public string[] secretSentences;
+
+    public int i;
+
+    private bool interacted = false;
+
+    void Start()
+    {
+        infoText = GameObject.Find("InfoText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       i = Random.Range(0, 10);
+    }
+
+    public void Info()
+    {
+
+        StartCoroutine(ShowInfo(messages[i], 3.0f));
+    }
+
+    public void PickUp()
+    {
+        Debug.Log("You picked up " + this.gameObject.name);
+        this.gameObject.SetActive(false);
+    }
+
+    public void Talks()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(sentences);
+        //Debug.Log(gameObject.name + " has nothing to say to you");
+    }
+
+    public void SecretTalks()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(secretSentences);
+        //Debug.Log(gameObject.name + " has nothing to say to you");
+    }
+
+    IEnumerator ShowInfo(string message, float delay)
+    {
+        if (interacted == false)
+        {
+            interacted = true;
+            infoText.text = message;
+            yield return new WaitForSeconds(delay);
+            infoText.text = null;
+            interacted = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public GameObject currentInterObject = null;
+    public InteractionObject currentInterObjectScript = null;
+
+    public bool hasGem = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && currentInterObject == true)
+        {
+
+            if (currentInterObjectScript.pickup == true)
+            {
+                currentInterObjectScript.PickUp();
+            }
+
+            if (currentInterObjectScript.gemPickup == true)
+            {
+                currentInterObjectScript.PickUp();
+                hasGem = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && currentInterObjectScript.info == true)
+            {
+                currentInterObjectScript.Info();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && currentInterObjectScript.talks == true && hasGem == true)
+            {
+                currentInterObjectScript.SecretTalks();
+                hasGem = false;
+            }
+            else if (Input.GetKeyDown(KeyCode.Space) && currentInterObjectScript.talks == true)
+            {
+                currentInterObjectScript.Talks();
+            }
+
+            
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("InterObject") == true)
+        {
+            currentInterObject = other.gameObject;
+            currentInterObjectScript = currentInterObject.GetComponent<InteractionObject>();
+        }
+
+        if (other.CompareTag("Gem") == true)
+        {
+            currentInterObject = other.gameObject;
+            currentInterObjectScript = currentInterObject.GetComponent<InteractionObject>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("InterObject") == true)
+        {
+            currentInterObject = null;
+        }
+    }
+}
